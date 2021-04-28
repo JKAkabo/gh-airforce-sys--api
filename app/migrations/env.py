cef349d0dfc5bc43203b8dd975aae1cbf1d99c3a
Ceasar15 <ceasarkwadwo@gmail.com>
2021-04-28 16:42:08 +0000

    user authentication

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -43,7 +43,8 @@ def run_migrations_online():
     configuration = config.get_section(config.config_ini_section)
     configuration["sqlalchemy.url"] = get_url()
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        # config.get_section(config.config_ini_section),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
